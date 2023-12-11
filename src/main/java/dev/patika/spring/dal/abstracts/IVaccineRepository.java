d626f69a9f46cc9246e198a4338b4418f6b13363
Batuhan <980.batuhantoy@gmail.com>
2023-12-11 18:26:43 +0300

    v1

--- src/main/java/dev/patika/spring/dal/abstracts/IVaccineRepository.java
+++ src/main/java/dev/patika/spring/dal/abstracts/IVaccineRepository.java
@@ -12,10 +12,12 @@ import java.util.List;
 @Repository
 public interface IVaccineRepository extends JpaRepository<Vaccine,Long> {
 
-    //SELECT vaccine_protection_end<'2023-08-10' FROM tbl_vaccine WHERE vaccine_name='Başka Aşı' AND vaccine_code='D2' AND animal_id=5
-    @Query("SELECT v.protectionEndDate<:protectionStartDate FROM Vaccine v WHERE v.name=:name AND v.code=:code AND v.animal=:animal")
+    //ERROR: Null Exception -> @Query("SELECT v.protectionEndDate<:protectionStartDate FROM Vaccine v WHERE v.name=:name AND v.code=:code AND v.animal=:animal")
+    @Query("SELECT Count(*)=0 FROM Vaccine v WHERE v.protectionEndDate>:protectionStartDate AND v.name=:name AND v.code=:code AND v.animal=:animal")
     boolean isReadyForVaccine(String name, String code, Animal animal,LocalDate protectionStartDate);
 
+
+    //@Query("SELECT Count(*)=0 FROM Vaccine v WHERE v.id=:id AND v.protectionEndDate>:protectionStartDate AND v.name=:name AND v.code=:code AND v.animal=:animal")
     @Query("SELECT v.protectionEndDate<:protectionStartDate FROM Vaccine v WHERE v.id=:id AND v.name=:name AND v.code=:code AND v.animal=:animal")
     boolean isReadyForVaccine(Long id,String name, String code, Animal animal,LocalDate protectionStartDate);
 
